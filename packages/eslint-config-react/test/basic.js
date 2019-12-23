646feb0076e3bdf135410fa7fa31152bb9eb209a
Hans Jakob Emmel <hans.emmel@gmail.com>
2019-12-23 17:37:14 +1300

    Basic basic rules

--- packages/eslint-config-react/test/basic.js
+++ packages/eslint-config-react/test/basic.js
@@ -1,18 +1,22 @@
-var config = require('../')
-var test = require('tape')
+const config = require('../');
+const test = require('tape');
 
 test('test basic properties of config', function (t) {
-  t.ok(isString(config.parser))
-  t.ok(isObject(config.extends))
-  t.ok(isObject(config.env))
-  t.ok(isObject(config.parserOptions))
-  t.end()
-})
+    t.ok(isString(config.parser));
+    t.ok(isObject(config.extends));
+    t.ok(isObject(config.env));
+    t.ok(isObject(config.parserOptions));
+    t.ok(isObject(config.settings));
+    t.equal(config.plugins.length, 2);
+    t.equal(Object.keys(config.rules).length, 6);
+    t.equal(Object.keys(config).length, 7);
+    t.end()
+});
 
-function isString (str) {
-    return typeof str === 'string' && str !== null
+function isString(str) {
+    return str !== null && typeof str === 'string'
 }
 
-function isObject (obj) {
+function isObject(obj) {
     return typeof obj === 'object' && obj !== null
 }
