3da4b5d15f44e60a4b302bd15bb3b9652e89e77d
Hans Jakob Emmel <hans.emmel@gmail.com>
2019-12-23 17:21:15 +1300

    Adding more tests

--- packages/eslint-config-base/test/basic.js
+++ packages/eslint-config-base/test/basic.js
@@ -1,18 +1,47 @@
-var config = require('../')
-var test = require('tape')
+const config = require('../');
+const test = require('tape');
 
 test('test basic properties of config', function (t) {
-  t.ok(isString(config.parser))
-  t.ok(isObject(config.extends))
-  t.ok(isObject(config.env))
-  t.ok(isObject(config.parserOptions))
-  t.end()
-})
+    t.ok(isString(config.parser));
+    t.ok(isObject(config.extends));
+    t.ok(isObject(config.env));
+    t.ok(isObject(config.parserOptions));
+    t.equal(Object.keys(config).length, 4);
+    t.end()
+});
 
-function isString (str) {
-    return typeof str === 'string' && str !== null
+const defaultPlugins = [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+];
+
+test('Should extend default plugns', function (t) {
+    t.equal(config.extends.length, 4, "Default plugins loaded");
+    config.extends.forEach((item) => {
+        t.ok(defaultPlugins.includes(item), `Plugin ${item} added`);
+    });
+    t.end()
+});
+
+test('Should use default parser', function (t) {
+    t.equal(config.parser, "@typescript-eslint/parser", "Default parser loaded");
+    t.end()
+});
+
+test('Should use default parser options', function (t) {
+    t.equal(config.parserOptions.ecmaVersion, 2018, "Using default ecma version");
+    t.equal(config.parserOptions.sourceType, "module", "Using default source type");
+    t.end()
+});
+
+
+
+function isString(str) {
+    return typeof str === 'string'
 }
 
-function isObject (obj) {
-    return typeof obj === 'object' && obj !== null
+function isObject(obj) {
+    return obj !== null && typeof obj === 'object'
 }
